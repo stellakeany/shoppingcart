433ea8f7929a68b27ce28e165c0f3bfd1ccd8ec6
stellakeany <stella.keany@ucdconnect.ie>
2023-02-03 15:22:56 +0000

    Setup for tests
    Added setup to create new ByteArrayOutputStream for every test run

--- java/src/test/java/com/xgen/interview/ShoppingCartTest.java
+++ java/src/test/java/com/xgen/interview/ShoppingCartTest.java
@@ -2,6 +2,7 @@ package com.xgen.interview;
 
 import com.xgen.interview.Pricer;
 import com.xgen.interview.ShoppingCart;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -12,14 +13,20 @@ import static org.junit.Assert.assertEquals;
 
 public class ShoppingCartTest {
 
+    private ByteArrayOutputStream myOut;
+
+    @Before
+    public void setup(){
+        myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+    }
+
+
     @Test
     public void canAddAnItem() {
         ShoppingCart sc = new ShoppingCart(new Pricer());
 
         sc.addItem("apple", 1);
-
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
 
         sc.printReceipt();
         assertEquals(String.format("apple - 1 - €1.00%n"), myOut.toString());
@@ -31,9 +38,6 @@ public class ShoppingCartTest {
 
         sc.addItem("apple", 2);
 
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
-
         sc.printReceipt();
         assertEquals(String.format("apple - 2 - €2.00%n"), myOut.toString());
     }
@@ -44,9 +48,6 @@ public class ShoppingCartTest {
 
         sc.addItem("apple", 2);
         sc.addItem("banana", 1);
-
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
 
         sc.printReceipt();
 
@@ -62,9 +63,6 @@ public class ShoppingCartTest {
         ShoppingCart sc = new ShoppingCart(new Pricer());
 
         sc.addItem("crisps", 2);
-
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
 
         sc.printReceipt();
         assertEquals(String.format("crisps - 2 - €0.00%n"), myOut.toString());
